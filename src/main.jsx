806c857232cb65b5cfaa639e849399337654c8fb
AnetteeYunuen <anetteyunuenruizm@gmail.com>
2025-09-30 09:58:40 -0600

    Cambios en main

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import './app.css'
+import './App.css'
 
 // Registrar Service Worker
 if ('serviceWorker' in navigator) {
